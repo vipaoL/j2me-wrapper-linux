5497fa8ebf929d1fe3091816913bf5d775f2ba39
vipaol <vipaolmail@gmail.com>
2022-09-18 22:22:21 +0400

    fix setting screen resolution

--- emu.sh
+++ emu.sh
@@ -5,5 +5,5 @@ SCALE=2
 cd ~/freej2me
 JAR_PATH=file://${HOME}/freej2me/apps/${APP_NAME}.jar
 echo ${JAR_PATH}
-#rm -r ../freej2me/config
+rm ../freej2me/config/*/game.conf # emulator ignores screen resolution provided in command-line arguments if it is already saved in the config. need to delete it if want to reconfigure the resolution
 java -cp freej2me.jar:lib/microemu-jsr-75.jar org.recompile.freej2me.FreeJ2ME ${JAR_PATH} $(( ${SCREEN_W}/${SCALE} )) $(( ${SCREEN_H}/${SCALE} )) ${SCALE}
