d7cb4b972656ec7965457c0b4e9f4e6ee75b4437
vipaol <vipaolmail@gmail.com>
2022-09-24 22:41:16 +0400

    fix adding config while installation

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd $(dirname $0)
+if [ ! -f wrapper-files/config/config.txt ]; then
+    cat wrapper-files/config/template-config.txt | sed "s#replace_with_install_dir#$(pwd)#g" > wrapper-files/config/config.txt
+fi
+
 source wrapper-files/config/config.txt
 cat $INSTALLATION_DIR/wrapper-files/shortcuts/template-add-j2me-jar.desktop | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > $INSTALLATION_DIR/wrapper-files/shortcuts/add-j2me-jar.desktop
 cat $INSTALLATION_DIR/wrapper-files/shortcuts/template-midlet-manager.desktop | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > $INSTALLATION_DIR/wrapper-files/shortcuts/midlet-manager.desktop
