84fc42167114ec0a7e40611f72e3c707cbf67b9f
vipaol <vipaolmail@gmail.com>
2023-02-10 01:59:31 +0400

    it's time to remove this temporary solution, because the emulator bug is fixed

--- wrapper-files/emu.sh
+++ wrapper-files/emu.sh
@@ -7,7 +7,5 @@ source config/window-dimensions.txt
 cd "${INSTALLATION_DIR}"
 JAR_PATH="file://$APPS_DIR/${APP_NAME}.jar"
 echo "${JAR_PATH}"
-sed -i '/width/d' config/*/game.conf # emulator ignores screen resolution provided in command-line arguments if it is already saved in the config. need to delete it if want to reconfigure the resolution
-sed -i '/height/d' config/*/game.conf
 #cat "config/$MOBAPP/game.conf"
 java -cp lib/*:freej2me.jar org.recompile.freej2me.FreeJ2ME "${JAR_PATH}" $(( ${W}/${SCALE} )) $(( ${H}/${SCALE} )) ${SCALE}
