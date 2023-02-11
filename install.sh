bc25cc04febc390738f8a02634c53fe5c9a83002
vipaol <vipaolmail@gmail.com>
2023-02-11 14:10:17 +0400

    moving shortcuts instead of symbolic linking to fix it on UT

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ source wrapper-files/config/config.txt
 cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-add-j2me-jar.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "$INSTALLATION_DIR/wrapper-files/shortcuts/add-j2me-jar.desktop"
 cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-midlet-manager.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "$INSTALLATION_DIR/wrapper-files/shortcuts/midlet-manager.desktop"
 mkdir -p ${SYSTEM_SHORTCUTS_DIR}
-ln -sf "${INSTALLATION_DIR}/wrapper-files/shortcuts/add-j2me-jar.desktop" "${SYSTEM_SHORTCUTS_DIR}/add-j2me-jar.desktop"
-ln -sf "${INSTALLATION_DIR}/wrapper-files/shortcuts/midlet-manager.desktop" "${SYSTEM_SHORTCUTS_DIR}/midlet-manager.desktop"
+mv -f "${INSTALLATION_DIR}/wrapper-files/shortcuts/add-j2me-jar.desktop" "${SYSTEM_SHORTCUTS_DIR}/add-j2me-jar.desktop"
+mv -f "${INSTALLATION_DIR}/wrapper-files/shortcuts/midlet-manager.desktop" "${SYSTEM_SHORTCUTS_DIR}/midlet-manager.desktop"
 cd wrapper-files && java -jar j2me-wrapper.jar get-dimensions
 
