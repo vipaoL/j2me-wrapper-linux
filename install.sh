70b5dc8d9ec87aa17ef24ace0f0856856062adfa
vipaol <vipaolmail@gmail.com>
2023-05-30 13:22:55 +0400

    fix getting screen dimensions during initialization

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ mkdir -p ${SYSTEM_SHORTCUTS_DIR}
 cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-add-j2me-jar.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "${SYSTEM_SHORTCUTS_DIR}/add-j2me-jar.desktop"
 cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-midlet-manager.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "${SYSTEM_SHORTCUTS_DIR}/midlet-manager.desktop"
 cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-midlet-manager-get-window-dims.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "${SYSTEM_SHORTCUTS_DIR}/midlet-manager-get-window-dims.desktop"
-cd bin && manager.sh get-dimensions
+cd bin && ./manager.sh get-dimensions
 
