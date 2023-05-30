f77df78f474b91e201530e284827e29d60db39dd
vipaol <vipaolmail@gmail.com>
2023-05-30 12:38:16 +0400

    revert a mistake

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd $(dirname $0)
-CONFIG_FILE=config/config.txt
+CONFIG_FILE=wrapper-files/config/config.txt
 if [ ! -s $CONFIG_FILE ]; then
-    cat config/template-config.txt | sed "s#replace_with_install_dir#$(pwd)#g" > $CONFIG_FILE
+    cat wrapper-files/config/template-config.txt | sed "s#replace_with_install_dir#$(pwd)#g" > $CONFIG_FILE
 fi
 
-source config/config.txt
-cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-add-j2me-jar.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "$INSTALLATION_DIR/wrapper-files/shortcuts/add-j2me-jar.desktop"
-cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-midlet-manager.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "$INSTALLATION_DIR/wrapper-files/shortcuts/midlet-manager.desktop"
-ln -sf "${INSTALLATION_DIR}/wrapper-files/shortcuts/add-j2me-jar.desktop" "${SYSTEM_SHORTCUTS_DIR}/add-j2me-jar.desktop"
-ln -sf "${INSTALLATION_DIR}/wrapper-files/shortcuts/midlet-manager.desktop" "${SYSTEM_SHORTCUTS_DIR}/midlet-manager.desktop"
-cd bin && java -jar j2me-wrapper.jar get-dimensions
-s
+source wrapper-files/config/config.txt
+mkdir -p ${SYSTEM_SHORTCUTS_DIR}
+cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-add-j2me-jar.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "${SYSTEM_SHORTCUTS_DIR}/add-j2me-jar.desktop"
+cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-midlet-manager.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "${SYSTEM_SHORTCUTS_DIR}/midlet-manager.desktop"
+cat "${INSTALLATION_DIR}/wrapper-files/shortcuts/template-midlet-manager-get-window-dims.desktop" | sed "s#replace_with_install_dir#$INSTALLATION_DIR#g" > "${SYSTEM_SHORTCUTS_DIR}/midlet-manager-get-window-dims.desktop"
+cd wrapper-files && java -jar j2me-wrapper.jar get-dimensions
+
